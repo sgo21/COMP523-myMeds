b34a9e8da357926fe3263665d2d6615955cdca0b
Duy <chipduy@live.unc.edu>
2021-03-12 01:19:40 -0500

    logo
    potential logo?

--- src/components/NavbarContainer.jsx
+++ src/components/NavbarContainer.jsx
@@ -14,31 +14,6 @@ import Dashboard from './Dashboard.js';
 
 const NavbarContainer = () => {
     return (
-    // <>  <AuthProvider>
-    //         <div >
-    //             <Navbar sticky="top" className="navbar" expand="md" collapseOnSelect>
-                   
-    //                 <Navbar.Toggle></Navbar.Toggle>
-    //                 <Navbar.Collapse id="responsive-navbar-nav">
-    //                     <Nav className="ml-auto roboto">
-    //                         <Nav.Link href="/">Home</Nav.Link>
-    //                         {/* <Nav.Link href="/my-profile">My Profile</Nav.Link> */}
-    //                         <Nav.Link href="/LogIn">Log In</Nav.Link>
-    //                         <Nav.Link href="/SignUp">Sign Up</Nav.Link>
-    //                     </Nav>
-    //                 </Navbar.Collapse>
-    //             </Navbar>
-    //         </div>
-    //         <BrowserRouter>
-    //             <Switch id="bodyContent">
-    //                 <Route exact path="/" component={Home} />
-    //                 <Route exact path="/LogIn" component={LogIn} />
-    //                 <Route exact path="/SignUp" component={Signup} />
-    //                 <PrivateRoute exact path="/my-profile"component={MyProfile}></PrivateRoute>
-    //             </Switch>
-    //         </BrowserRouter>
-    //     </AuthProvider>
-    // </>
          <div >
              <Header/>
             <Navbar sticky="top" className="navbar" expand="md" collapseOnSelect>
