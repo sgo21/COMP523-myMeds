a96152dd2d163c10b078f9579f4526f40593b0fb
Sungdong Kim <40084583+SungMIA@users.noreply.github.com>
2021-04-08 13:24:27 -0400

    Revert

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,39 @@
+import React, {} from 'react';
+import {Button, Card, Media} from "react-bootstrap"
+import { useHistory } from "react-router-dom";
+import Rating from '@material-ui/lab/Rating';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { ReactComponent as Logo } from '../img/logo.svg';
+import '../css/Reviews.css';
+
+function Reviews ({ review }) {
+    const name = review.name;
+    const age = review.age;
+    const sex = review.sex;
+    const race = review.race;
+    const symptom = review.symptom;
+    const rating = review.rating;
+    const reviewDescrip  = review.review;
+
+
+    const history = useHistory();
+
+
+    return (<div>
+            <Media as="li">
+                <Logo width="40" height="40" className="logo d-inline-block mb-3 mr-3"/> 
+                
+                <Media.Body>
+                    <h5>{name}</h5>
+                    <Box component="fieldset" borderColor="transparent">
+                        <Typography component="legend">Demographic: {age}, {sex}, {race}</Typography>
+                        <Rating name="read-only" value={rating} readOnly />
+                        <p> {reviewDescrip} </p>
+                    </Box>
+
+                </Media.Body>
+            </Media>
+    </div>)}
+
+export default Reviews;        
