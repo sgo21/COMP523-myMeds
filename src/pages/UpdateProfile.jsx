7d4c8f73849fc861c4361a7db24acadf6b88b68c
usmanraja9 <43099963+usmanraja9@users.noreply.github.com>
2021-04-14 11:49:39 -0400

    set up the update page

--- src/pages/UpdateProfile.jsx
+++ src/pages/UpdateProfile.jsx
@@ -1,73 +1,93 @@
-import React, { useState, useEffect } from "react"
-import { Card, Button, Alert } from "react-bootstrap"
+import React, { useState, useEffect, useRef } from "react";
 import { useAuth } from "../contexts/AuthContext"
-import { Link, useHistory } from "react-router-dom"
-import '../css/MyProfile.css';
 import {db} from '../firebase'
+import { Form, Button, Card } from "react-bootstrap"
+import '../css/Home.css';
+import '../css/MedPage.css';
 import NavbarContainer from '../components/NavbarContainer'
 
-export default function UpdateProfile() {
-  const [error, setError] = useState("")
-  const { currentUser, logout } = useAuth()
-  const history = useHistory()
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [race, setRace] = useState("");
-  const [sex, setSex] = useState("");
+export default function RequestForm() {
+    const { currentUser } = useAuth()
+    const [name, setName] = useState("");
+    const [race, setRace] = useState("");
+    const sexRef = useRef();
+    const [age, setAge] = useState("");
+
+
+    useEffect(() => {
+      async function getUserData() {
+        // You can await here
+        const userDoc = await db.collection('User').doc(currentUser.email).get();
+        setName(userDoc.data().name);
+        setRace(userDoc.data().race);
+        setAge(userDoc.data().age);
+      }
+        getUserData();
+    }, []);  
   
-
-  useEffect(() => {
-    async function getUserData() {
-      // You can await here
-      const userDoc = await db.collection('User').doc(currentUser.email).get();
-      setName(userDoc.data().name);
-      setRace(userDoc.data().race);
-      setSex(userDoc.data().sex);
-      setAge(userDoc.data().age);
-    }
-      getUserData();
-  });  
-
-  async function handleLogout() {
-    setError("")
-
-    try {
-      await logout()
-      history.push("/")
-    } catch {
-      setError("Failed to log out")
-    }
-  }
-
-  return (
-    <>
+      const handleSubmit =(e) => {
+         e.preventDefault();
+  
+        //doc(currentUser.email).set
+        db.collection('User').doc(currentUser.email).set({
+          name: name,
+          sex:sexRef.current.value,
+          race: race,
+          age: age,
+        })
+        .then(() => {
+          alert('Got It(');
+          //console.log(name, age, sex, race, symtpom, race, review, rating, ids);
+        })
+        .catch(error => {
+          alert(error.mesage);
+        })
+      };
+      
+    return (
       <div>
-        <NavbarContainer/>
+        <div>
+          <NavbarContainer/>
+        </div>
+        <Card className="review text-left m-5 mx-auto border-0">
+          <Card.Body>
+            <h2 className="text-center mb-4">Request</h2>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="Name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control 
+                  placeholder={name}
+                  value={name} 
+                  onChange={(e) => setName(e.target.value)}/>
+              </Form.Group>
+              <Form.Group controlId="exampleForm.ControlSelect1">
+                <Form.Label>Sex</Form.Label>
+                <Form.Control as="select" ref={sexRef}>
+                  <option>Female</option>
+                  <option>Male</option>
+                  <option>Other</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group id="Race">
+                <Form.Label>Race</Form.Label>
+                <Form.Control
+                  placeholder={race}
+                  value={race} 
+                  onChange={(e) => setRace(e.target.value)}/>
+              </Form.Group>
+              <Form.Group id="Age">
+                <Form.Label>Age</Form.Label>
+                <Form.Control
+                  placeholder={age}
+                  value={age} 
+                  onChange={(e) => setAge(e.target.value)}/>
+              </Form.Group>
+              <Button className="w-100" type="submit">
+                Send Request
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
       </div>
-      <Card className="my-profile text-left m-5 mx-auto border-0">
-        <Card.Body>
-          <h2 className="text-center mb-4">Update Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          <br/>
-          <strong>Name:</strong> {name}
-          <br/>
-          <strong>Age:</strong> {age}
-          <br/>
-          <strong>Race:</strong> {race}
-          <br/>
-          <strong>Sex:</strong> {sex}
-
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out
-        </Button>
-      </div>
-    </>
-  )
-}
+    );
+  }
